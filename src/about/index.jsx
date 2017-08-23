1e85b30dbf368f465f8cce6a259a09032dc64083
Thomas 'Taldrain' Mariaux <taldrain@gmail.com>
2017-08-23 18:11:18 +0200

    add info in the about page

--- src/about/index.jsx
+++ src/about/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import Typography from 'material-ui/Typography';
 import Grid from 'material-ui/Grid';
+import Card, { CardContent } from 'material-ui/Card';
+
+const TextDisplay = require('../components/display/text.jsx');
 
 const styles = {
   root: {
@@ -16,7 +20,24 @@ class About extends React.Component {
     return (
       <Grid container direction="row" justify="center" align="center" style={styles.root}>
         <Grid item xs={8}>
-          About
+          <Card>
+            <CardContent>
+              <Typography type="title">
+                <TextDisplay value="about.Title" />
+              </Typography>
+            </CardContent>
+            <CardContent>
+              <Typography>
+                <TextDisplay value="about.Description" />
+              </Typography>
+              <Typography>
+                <TextDisplay value="about.Type of entry" />
+              </Typography>
+              <Typography>
+                <TextDisplay value="about.Example" />
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
     );
