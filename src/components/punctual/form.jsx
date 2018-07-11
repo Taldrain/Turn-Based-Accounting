b4b449cc5a2697a7477a0aef51c6fdd73d734fc0
Thomas 'Taldrain' Mariaux <taldrain@gmail.com>
2018-07-11 18:59:53 +0200

    add form autocomplete off around form components

--- src/components/punctual/form.jsx
+++ src/components/punctual/form.jsx
@@ -7,35 +7,37 @@ import BalanceRadio from '../balance-radio/index';
 
 function Form(props) {
   return (
-    <Grid container direction="column" justify="space-around" align="stretch" spacing={32}>
-      <Grid item>
-        <TextField
-          id="name"
-          label="Name"
-          value={props.name}
-          onChange={ev => props.onNewValue('name', ev.target.value)}
-          autoFocus
-          fullWidth
-        />
+    <form autoComplete="off">
+      <Grid container direction="column" justify="space-around" align="stretch" spacing={32}>
+        <Grid item>
+          <TextField
+            id="name"
+            label="Name"
+            value={props.name}
+            onChange={ev => props.onNewValue('name', ev.target.value)}
+            autoFocus
+            fullWidth
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="amount"
+            label="Amount"
+            value={props.amount}
+            onChange={ev => props.onNewValue('amount', parseFloat(ev.target.value || 0, 10))}
+            type="number"
+            min="0"
+            fullWidth
+          />
+        </Grid>
+        <Grid item>
+          <BalanceRadio
+            onChange={value => props.onNewValue('isPositive', value === '+')}
+            isPositive={props.isPositive}
+          />
+        </Grid>
       </Grid>
-      <Grid item>
-        <TextField
-          id="amount"
-          label="Amount"
-          value={props.amount}
-          onChange={ev => props.onNewValue('amount', parseFloat(ev.target.value || 0, 10))}
-          type="number"
-          min="0"
-          fullWidth
-        />
-      </Grid>
-      <Grid item>
-        <BalanceRadio
-          onChange={value => props.onNewValue('isPositive', value === '+')}
-          isPositive={props.isPositive}
-        />
-      </Grid>
-    </Grid>
+    </form>
   );
 }
 
--- src/components/recurrent/form.jsx
+++ src/components/recurrent/form.jsx
@@ -8,61 +8,63 @@ import TypeMenu from '../type-menu/index';
 
 function Form(props) {
   return (
-    <Grid container direction="column" justify="space-around" align="stretch" spacing={32}>
-      <Grid item>
-        <TextField
-          id="name"
-          label="Name"
-          value={props.name}
-          onChange={ev => props.onNewValue('name', ev.target.value)}
-          autoFocus
-          fullWidth
-        />
+    <form autoComplete="off">
+      <Grid container direction="column" justify="space-around" align="stretch" spacing={32}>
+        <Grid item>
+          <TextField
+            id="name"
+            label="Name"
+            value={props.name}
+            onChange={ev => props.onNewValue('name', ev.target.value)}
+            autoFocus
+            fullWidth
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="amount"
+            label="Amount"
+            value={props.amount}
+            onChange={ev => props.onNewValue('amount', parseFloat(ev.target.value, 10))}
+            type="number"
+            min="0"
+            fullWidth
+          />
+        </Grid>
+        <Grid item>
+          <BalanceRadio
+            onChange={value => props.onNewValue('isPositive', value === '+')}
+            isPositive={props.isPositive}
+          />
+        </Grid>
+        <Grid item>
+          <TypeMenu
+            onChange={value => props.onNewValue('type', value)}
+            type={props.type}
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="startDate"
+            helperText="Start date"
+            value={props.startDate}
+            onChange={ev => props.onNewValue('startDate', ev.target.value)}
+            type="date"
+            fullWidth
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="endDate"
+            helperText="End date"
+            value={props.endDate}
+            onChange={ev => props.onNewValue('endDate', ev.target.value)}
+            type="date"
+            fullWidth
+          />
+        </Grid>
       </Grid>
-      <Grid item>
-        <TextField
-          id="amount"
-          label="Amount"
-          value={props.amount}
-          onChange={ev => props.onNewValue('amount', parseFloat(ev.target.value, 10))}
-          type="number"
-          min="0"
-          fullWidth
-        />
-      </Grid>
-      <Grid item>
-        <BalanceRadio
-          onChange={value => props.onNewValue('isPositive', value === '+')}
-          isPositive={props.isPositive}
-        />
-      </Grid>
-      <Grid item>
-        <TypeMenu
-          onChange={value => props.onNewValue('type', value)}
-          type={props.type}
-        />
-      </Grid>
-      <Grid item>
-        <TextField
-          id="startDate"
-          helperText="Start date"
-          value={props.startDate}
-          onChange={ev => props.onNewValue('startDate', ev.target.value)}
-          type="date"
-          fullWidth
-        />
-      </Grid>
-      <Grid item>
-        <TextField
-          id="endDate"
-          helperText="End date"
-          value={props.endDate}
-          onChange={ev => props.onNewValue('endDate', ev.target.value)}
-          type="date"
-          fullWidth
-        />
-      </Grid>
-    </Grid>
+    </form>
   );
 }
 
