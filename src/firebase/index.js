718727a60473b7bbac4c865ea209d883f43605aa
Thomas 'Taldrain' Mariaux <taldrain@gmail.com>
2018-10-10 15:35:59 +0200

    fix firebase login

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,21 +1,9 @@
-let firebaseWrap;
+const firebase = require('firebase/app');
+require('firebase/auth');
+require('firebase/firestore');
 
-if (process.env.NODE_ENV !== 'production') {
-  // eslint-disable-next-line global-require
-  const Config = require('./config.json');
+const config = require('./config.json');
 
-  // eslint-disable-next-line
-  firebaseWrap = require('firebase/app')
-  // eslint-disable-next-line
-  require('firebase/auth')
-  // eslint-disable-next-line
-  require('firebase/firestore')
-
-  firebaseWrap.initializeApp(Config);
-} else {
-  firebaseWrap = window.firebase;
-}
-
-const firebase = firebaseWrap;
+firebase.initializeApp(config);
 
 export default firebase;
