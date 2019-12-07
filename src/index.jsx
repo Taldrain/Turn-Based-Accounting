713f0230647481965f41b6311e8a95273461750e
Thomas 'Taldrain' Mariaux <taldrain@gmail.com>
2019-12-07 23:03:11 +0100

    update dependencies - add React.StrictMode

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { ThemeProvider } from '@material-ui/core/styles';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
@@ -15,13 +15,15 @@ import reducers from './reducers/index';
 const store = createStore(reducers);
 
 ReactDOM.render(
-  <MuiThemeProvider theme={theme}>
+  <ThemeProvider theme={theme}>
     <CssBaseline />
     <Provider store={store}>
       <BrowserRouter>
-        <Root />
+        <React.StrictMode>
+          <Root />
+        </React.StrictMode>
       </BrowserRouter>
     </Provider>
-  </MuiThemeProvider>,
+  </ThemeProvider>,
   document.getElementById('tba-app'),
 );
